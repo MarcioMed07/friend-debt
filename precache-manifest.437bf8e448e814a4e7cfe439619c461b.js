7b8f540e99027767391933be63f736346364a246
Márcio Medeiros <marciomed07@gmail.com>
2020-09-07 20:01:57 -0300

    Updates

---
+++ precache-manifest.437bf8e448e814a4e7cfe439619c461b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd31736a85f57e6936aefba8992d609",
+    "url": "/Friend-Debt/index.html"
+  },
+  {
+    "revision": "523c22e39ca292d14a56",
+    "url": "/Friend-Debt/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "4da4b6959816ac1e1409",
+    "url": "/Friend-Debt/static/css/main.b67b7bfb.chunk.css"
+  },
+  {
+    "revision": "523c22e39ca292d14a56",
+    "url": "/Friend-Debt/static/js/2.55490022.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/Friend-Debt/static/js/2.55490022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da4b6959816ac1e1409",
+    "url": "/Friend-Debt/static/js/main.f5ee2c5a.chunk.js"
+  },
+  {
+    "revision": "dcb169d22e7fd3cbf5ef",
+    "url": "/Friend-Debt/static/js/runtime-main.ae1eccd7.js"
+  }
+]);
