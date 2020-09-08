f1eba96b1d0fae0cbb04f88c5adb736b29f52614
Márcio Medeiros <marciomed07@gmail.com>
2020-09-07 21:37:56 -0300

    Updates

---
+++ precache-manifest.c8ec755cde491da991bba481941c09d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0759909257c12ea0909178aa0bbbf846",
+    "url": "/friend-debt/index.html"
+  },
+  {
+    "revision": "13bb520904a650af791a",
+    "url": "/friend-debt/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "2d8815aaed19a1e5e456",
+    "url": "/friend-debt/static/css/main.a5e14f19.chunk.css"
+  },
+  {
+    "revision": "13bb520904a650af791a",
+    "url": "/friend-debt/static/js/2.c12a26b7.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/friend-debt/static/js/2.c12a26b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d8815aaed19a1e5e456",
+    "url": "/friend-debt/static/js/main.47320bcd.chunk.js"
+  },
+  {
+    "revision": "f618146fe642d75b4efe",
+    "url": "/friend-debt/static/js/runtime-main.b32dcf66.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Friend-Debt/precache-manifest.0bc29b5abd4655de835047475da1c13f.js"
+  "/friend-debt/precache-manifest.c8ec755cde491da991bba481941c09d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Friend-Debt/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/friend-debt/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
